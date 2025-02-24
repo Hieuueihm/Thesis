2ea4856982cf59d9e969f9274cf208442ea06929
Hieu <gbmhieu123@gmail.com>
2025-02-24 14:21:58 +0700

    modify ip following axistream 4

--- src/modules/top_module/top_module.v
+++ src/modules/top_module/top_module.v
@@ -4,25 +4,36 @@ module top_module #(
 ) (
     input clk,
     input rst_n,
-    input [7:0] grayscale_i,
     input start_i,
+    // slave interface
+    input [7:0] grayscale_i,
     input i_valid,
+    output o_data_ready,
+    // master interface 
     output [15:0] histogram_o,
     output o_valid,
+    input i_data_ready,
+    // interrupt
     output o_intr
 );
+
+  wire axis_prog_full;
+  assign o_data_ready = ~axis_prog_full;
 
 
   wire finish;
   wire read_finish;
   wire read_en;
+  wire [15:0] histogram;
+  wire o_histogram_valid;
+  wire o_intr_o;
   top_module__controller inst_top_module__controller (
       .clk        (clk),
       .rst_n      (rst_n),
       .finish_i   (finish),
       .start_i    (start_i),
       .read_finish(read_finish),
-      .o_intr     (o_intr),
+      .o_intr     (o_intr_o),
       .read_en    (read_en)
   );
   top_module__datapath #(
@@ -34,12 +45,56 @@ module top_module #(
       .grayscale_i(grayscale_i),
       .i_valid    (i_valid),
       .read_en    (read_en),
-      .histogram_o(histogram_o),
-      .o_valid    (o_valid),
+      .histogram_o(histogram),
+      .o_valid    (o_histogram_valid),
       .finish     (finish),
       .read_finish(read_finish)
   );
 
+  output_buffer_ip OB (
+      .wr_rst_busy  (),                   // output wire wr_rst_busy
+      .rd_rst_busy  (),                   // output wire rd_rst_busy
+      .s_aclk       (clk),
+      .s_aresetn    (rst_n),
+      // s_axis
+      .s_axis_tvalid(o_histogram_valid),
+      .s_axis_tready(),
+      .s_axis_tdata (histogram),
+      // m_axis
+      .m_axis_tvalid(o_valid),            // output wire m_axis_tvalid
+      .m_axis_tready(i_data_ready),       // input wire m_axis_tready
+      .m_axis_tdata (histogram_o),
+
+      .axis_prog_full(axis_prog_full)
+  );
+  wire o_intr_q;
+  dff #(
+      .WIDTH(1)
+  ) dff_o_intr (
+      .clk(clk),
+      .rst_n(rst_n),
+      .D(o_intr_o),
+      .Q(o_intr_q)
+  );
+
+  dff #(
+      .WIDTH(1)
+  ) dff_o_intr_q1 (
+      .clk(clk),
+      .rst_n(rst_n),
+      .D(o_intr_q),
+      .Q(o_intr)
+  );
+  //   integer file;
+  //   always @(posedge clk) begin
+  //     if (~rst_n) begin
+  //       file = $fopen("D:\\Thesis\\codetest\\python\\histogram_verilog_prev.txt", "w");
+  //     end else if (o_histogram_valid) begin
+  //       $fwrite(file, "%d\n", histogram);
+  //     end else if (o_intr) begin
+  //       $fclose(file);
+  //     end
+  //   end
 
 
 endmodule
--- src/test_benches/test/test_top_tb_with_image.v
+++ src/test_benches/test/test_top_tb_with_image.v
@@ -12,10 +12,12 @@ module test_top_tb_with_image ();
   reg [7:0] grayscale_i;
   reg i_valid;
   reg start_i;
+  reg i_data_ready;
 
   wire [15:0] histogram_o;
   wire o_valid;
   wire o_intr;
+  wire o_data_ready;
 
   integer i, j;
 
@@ -66,12 +68,13 @@ module test_top_tb_with_image ();
   endtask
 
   initial begin
-    rst_n       = 1'b0;
-    start_i     = 1'b0;
-    i_valid     = 1'b0;
-    grayscale_i = 8'b0;
+    rst_n        = 1'b0;
+    start_i      = 1'b0;
+    i_valid      = 1'b0;
+    grayscale_i  = 8'b0;
+    i_data_ready = 1'b0;
 
-    file_out    = $fopen("D:\\Thesis\\codetest\\python\\histogram_verilog.txt", "w");
+    file_out     = $fopen("D:\\Thesis\\codetest\\python\\histogram_verilog.txt", "w");
 
     if (file_out == 0) begin
       $display("Error: Could not open output files.");
@@ -85,6 +88,7 @@ module test_top_tb_with_image ();
     rst_n = 1'b1;
     #(`clk_period);
     start_i = 1'b1;
+    i_data_ready = 1'b1;
     #(`clk_period);
     start_i = 1'b0;
     i_valid = 1'b1;
@@ -112,9 +116,11 @@ module test_top_tb_with_image ();
       .rst_n(rst_n),
       .grayscale_i(grayscale_i),
       .i_valid(i_valid),
+      .o_data_ready(o_data_ready),
       .start_i(start_i),
       .histogram_o(histogram_o),
       .o_valid(o_valid),
+      .i_data_ready(i_data_ready),
       .o_intr(o_intr)
   );
 
